1d0fba56f06ad66d1c6c31634e4a7321e7ac9955
mapyr0110 <n_abish@kbtu.kz>
2026-03-29 03:14:47 +0500

    lab9

--- lab8/api/urls.py
+++ lab8/api/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail),
-    path('categories/', views.category_list),
-    path('categories/<int:id>/', views.category_detail),
-    path('categories/<int:id>/products/', views.category_products),
-]
+# urlpatterns = [
+#     path('products/', views.product_list),
+#     path('products/<int:id>/', views.product_detail),
+#     path('categories/', views.category_list),
+#     path('categories/<int:id>/', views.category_detail),
+#     path('categories/<int:id>/products/', views.category_products),
+# ]
+
+router = DefaultRouter()
+
+router.register('categories', views.CategoryViewSet)
+router.register('products', views.ProductViewSet)
+
+urlpatterns = router.urls
